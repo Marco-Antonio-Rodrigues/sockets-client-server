9d82a718af14df905253c691ee93a8236ec0d7d3
levicastr <63020217+levicastr@users.noreply.github.com>
2023-10-02 12:59:10 -0300

    Update function_server.py
    atualização

--- server/function_server.py
+++ server/function_server.py
@@ -1,21 +1,18 @@
 from os import listdir,path
 import os
 
-
 def listar_arquivos():
-    diretorio = os.path.abspath()
-    arquivo = os.path.join(diretorio, "nome_arquivo")
+    diretorio = os.path.abspath("nome_arquivo")
+    return os.listdir(diretorio)
 
-    return listdir(arquivo)
-    
-def enviar_arquivos():
-    diretorio = os.path.abspath()
-    arquivo = os.path.join(diretorio, "nome_arquivo", nome_arquivo)
+def enviar_arquivos(nome_arquivo):
+    diretorio = os.path.abspath("nome_arquivo")
+    caminho_arquivo = os.path.join(diretorio, nome_arquivo)
 
-    with open(arquivo, "rb") as files:
-        for data in files.readline():
-           return(data)
-        return ("arquivo enviado!")
-       
-def finalizar_conexao():
-    return finalizar_conexao.close()
+    with open(caminho_arquivo, "rb") as arquivo:
+        conteudo = arquivo.read()
+        return conteudo
+
+def finalizar_conexao(conexao):
+    conexao.close()
+    return "Conexão finalizada com sucesso."
